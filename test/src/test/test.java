41aba53be8a5255b10fe2883c58d5f1aba674fee
hit0207 <JXB534LJ137@163.com>
2018-05-07 11:02:06 +0800

    测试commit

---
+++ test/src/test/test.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.io.File;
+
+public class test {
+
+	public static void main(String[] args) {
+		File file = new File(System.getProperty("user.dir"));
+		System.out.println(System.getProperty("user.dir"));
+		String path = file.getAbsolutePath().replace('\\', '/');
+		System.out.println(path);
+			   path = path.substring(0, path.indexOf('/'));
+		System.out.println("test"+path);
+	}
+}
